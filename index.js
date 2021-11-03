a6d41a8b13f20b31c00532cdfeb5e45acbfadda5
Alecsi Montanari <alecsimontanari@gmail.com>
2021-11-03 12:14:30 -0300

    Added mysql connection

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 const customExpress = require('./config/customExpress');
-const app = customExpress();
-app.listen(3000, () => console.log('Serverup'));
+const conexao = require('./infra/conexao');
+
+conexao.connect(erro => {
+    if(erro){
+        console.log('erro',erro);
+    } else {
+        console.log('Database Connected successfully');  
+        const app = customExpress();
+        app.listen(3000, () => console.log('Serverup'));
+    }
+});
+
 
