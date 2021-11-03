f643b2c8bb7f58f12ea46bbe19e0549983de518d
Alecsi Montanari <alecsimontanari@gmail.com>
2021-11-03 12:00:34 -0300

    Adding express parser

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -2,4 +2,8 @@ module.exports = app => {
     app.get('/atendimentos', (req, res)=>{
         res.send('você está na rota de atendimentos');
     })
+    app.post('/atendimentos', (req, res)=>{
+        console.log('req.body',req.body);
+        res.send('você está em atendimento, fazendo post');
+    })
 }
