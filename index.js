766097f058db173afe594512db46e51b1d2ad71a
Alecsi Montanari <alecsimontanari@gmail.com>
2021-11-03 11:20:05 -0300

    Added nodemom and configure npm start

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const express = require('express');
 const app = express();
 
 app.listen(3000, () => console.log('Serverup'));
+
+app.get('/', (req, res)=>{
+    res.send('Server up and running');
+})
